0d96b20903ce773dcc2622ddc240a6624550f2ba
JacobSmith <jacob13smith@gmail.com>
2019-01-25 19:57:34 -0800

    added my view
    Signed-off-by: JacobSmith <jacob13smith@gmail.com>

---
+++ app/Views/jacob.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Welcome to Jacob's page</title>
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+  </head>
+  <body>
+  	<style>
+  		body {
+  			text-align: center;
+  		}
+		header {
+			font-family: 'Source Sans Pro', sans-serif;
+			font-size: 1.75em;
+			letter-spacing: 6px;
+			font-weight: 600;
+			line-height: 1.5em;
+      display: inline-block;
+		}
+    header p:before {
+      content: '';
+      display: block;
+      border-top: solid 1px;
+      margin: 4px 0 0.8em 0;
+    }
+    header p {
+      border-top: solid 1px;
+      font-size: 1em;
+      text-transform: uppercase;
+      letter-spacing: 3px;
+      margin-top: 0.35em;
+    }
+	</style>
+    <header>
+            <h1>Jacob Smith</h1>
+            <p>BCIT Computer Systems Student</p>
+        </header>
+  </body>
+</html>
